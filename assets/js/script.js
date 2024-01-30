b11754326b4b0e0be447c5454fc5f258da1c1807
Ross <rossycharlton@googlemail.com>
2024-01-30 23:09:25 +0000

    Added comments to js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,48 @@
-$(document).ready(function() {
-    var APIKey = "b417359692c82a9e4c2d44064c15cf54";
-    var baseURL = "https://api.openweathermap.org/data/2.5/"
+$(document).ready(function() { //Wait untill everything is loaded and then run this file
+    var APIKey = "b417359692c82a9e4c2d44064c15cf54";    //API key for OpenWeatherMap
+    var baseURL = "https://api.openweathermap.org/data/2.5/"    //Base URL for OpenWeatherMap weather
 
-    function fetchData(city, endpoint, successCallback, errorCallback) {
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`)
-        .then(response => {
+    function fetchData(city, endpoint, successCallback, errorCallback) {    //Function that is used to fetch the data for the forcast and city lat & long
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`) //retreve lat and long for a city name
+        .then(response => { //if responce isnt okay then throw and error
             if (!response.ok) {
                 throw new Error('Something went wrong :(');
             }
-        return response.json();
+        return response.json(); //return the responce in a parsed form
         })
         .then(data => {
-            var latitude = data[0].lat;
+            var latitude = data[0].lat; //extract lat and long fromn the reponce
             var longitude = data[0].lon;
-            return fetch(`${baseURL}${endpoint}?lat=${latitude}&lon=${longitude}&units=metric&appid=${APIKey}`)
+            return fetch(`${baseURL}${endpoint}?lat=${latitude}&lon=${longitude}&units=metric&appid=${APIKey}`) //use the lat and long to retrive forcast info. 
             })
-        .then(response => {
+        .then(response => { //if responce isnt okay then throw and error
             if (!response.ok) {
                 throw new Error('Something went wrong :(');
             }
-            return response.json();
+            return response.json(); //return the responce in a parsed form
         })
-        .then(data => successCallback(data))
-        .catch(error => errorCallback(error));
+        .then(data => successCallback(data))   //call the passed in successCallback function
+        .catch(error => errorCallback(error)); //if an error is thrown the call the passed In Error Function
     }
 
-    function showError(e){
-        $("#today").empty();
+    function showError(e){ //function to dispay an error message when  there is an error 99.9% of the time this will be because a invalid city was enterd
+        $("#today").empty(); //clear the currently displayed forcasts
         $("#forecast").empty();
-        $("#forecast").append($("<h2>", {class: "m-3", text: "Something went wrong, did you enter a valid city?"}))
+        $("#forecast").append($("<h2>", {class: "m-3", text: "Something went wrong, did you enter a valid city?"})) //dispaly a user friendly error message to the page
     }
 
-    function build5DayForcast(data){
+    function build5DayForcast(data){  //function to format the data from the api in a way that can be used to display a 5 day forcast 
         var fiveDayForcast = []
         var unixTimeStampsToBuilForcast = []
-        var CurrentDayItteration = dayjs().add(1, "day").set('hour', 12).set('minute', 0).set('second', 0);
-        for(let i=0; i <=5; i++){
-            unixTimeStampsToBuilForcast.push(CurrentDayItteration.unix())
-            CurrentDayItteration = CurrentDayItteration.add(1, "day").set('hour', 12).set('minute', 0).set('second', 0);
+        var CurrentDayItteration = dayjs().add(1, "day").set('hour', 12).set('minute', 0).set('second', 0); //get the current time, add 1 day onto it and set the hour to 12, this will mean we always get the forcast for midday
+        for(let i=0; i <=5; i++){ //loop 5 times to get 5 unixtime stamps (1 per day) for the next 5 days at midday
+            unixTimeStampsToBuilForcast.push(CurrentDayItteration.unix()) //add that unix time stamp to an array, ready to be compirted in the next step
+            CurrentDayItteration = CurrentDayItteration.add(1, "day").set('hour', 12).set('minute', 0).set('second', 0); //add one day onto the CurrentDayItteration
         }
 
-        data.list.forEach(element => {
+        data.list.forEach(element => { //for each forcast that the api sent look thru each one and extract the ones that we need. (1 per day, as 12 noon, for the next 5 days) 
             if(unixTimeStampsToBuilForcast.includes(element.dt)){
-                fiveDayForcast.push(element)
+                fiveDayForcast.push(element) //if the unix date time matches then add it the to fiveDayForcast array
             }
         });
 
@@ -50,20 +50,20 @@ $(document).ready(function() {
             fiveDayForcast.push(data.list[data.list.length-1])     //If this is the case then just make the 5th day the last element in the retuned data. it will always be the correct day but it might not always be at 12 noon
         }
         
-        display5DayForcast(fiveDayForcast)
+        display5DayForcast(fiveDayForcast) // call the function that will display the 5 day forcast we just created
         return
     }
 
-    function display5DayForcast(data){
-        $("#forecast").empty();
-        var sectionTitle = $("<h3>", {class: "mt-4", text: "Five Day Forecast:"})
-        $($("#forecast").append(sectionTitle))
-        $.each(data, function(index, dayForcast){
-            var {temp, humidity} = dayForcast.main
-            var {speed: windSpeed} = dayForcast.wind
-            var iconLink = `https://openweathermap.org/img/wn/${dayForcast.weather[0].icon}@2x.png`
-            var forcastCard = $("<div>", {
-                class:"card m-2 col-sm-12 col-md",
+    function display5DayForcast(data){  //function to display the 5 day forcast to the page
+        $("#forecast").empty(); //remove anything thats currently in the forecast div
+        var sectionTitle = $("<h3>", {class: "mt-4", text: "Five Day Forecast:"}) //add h3 title for this section
+        $($("#forecast").append(sectionTitle))  //add the title to the page
+        $.each(data, function(index, dayForcast){   //for each day create a forecast card
+            var {temp, humidity} = dayForcast.main  //destructute temp & humidity from dayForcast.main
+            var {speed: windSpeed} = dayForcast.wind    //destructute speed as windSpeed
+            var iconLink = `https://openweathermap.org/img/wn/${dayForcast.weather[0].icon}@2x.png` //create the link for the weather icon
+            var forcastCard = $("<div>", { //create the parent div for the forecast card
+                class:"card m-2 col-sm-12 col-md", // add classes to the forecast card
                 html: `
                     <div class="card-body">
                     <h6 class="mb-1">${dayjs.unix(dayForcast.dt).format("dddd")}</h6>
@@ -78,25 +78,26 @@ $(document).ready(function() {
                     </ul>`,
             })
 
-            $("#forecast").append(forcastCard)
+            $("#forecast").append(forcastCard) //add forcast card to the forecast div
         })
     }
 
-    function displayTodayForcast(dayForcast){
-        if(dayForcast.cod !== 200) return
+    function displayTodayForcast(dayForcast){   //function to display the today forcecast to the page
+        if(dayForcast.cod !== 200) return //return if status code isnt ok
 
-        $("#today").empty();
-        var sectionTitle = $("<h3>", {class: "mt-4", text: "Today:"})
-        $($("#today").append(sectionTitle))
-        var {temp, humidity} = dayForcast.main
-        var {speed: windSpeed} = dayForcast.wind
-        var iconLink = `https://openweathermap.org/img/wn/${dayForcast.weather[0].icon}@2x.png`
-        //        <img class="card-img" src="..." alt="Card image">
-        var todayForcast = $("<div>", {
+        $("#today").empty(); //remove anything thats currently in the today div
+        var sectionTitle = $("<h3>", {class: "mt-4", text: "Today:"}) //create a title for the today section
+        $($("#today").append(sectionTitle)) //show the title
+        var {temp, humidity} = dayForcast.main //destructute temp & humidity from dayForcast.main
+        var {speed: windSpeed} = dayForcast.wind //destructute speed as windSpeed
+        var iconLink = `https://openweathermap.org/img/wn/${dayForcast.weather[0].icon}@2x.png`//create the link for the weather icon
+        // <img class="card-img" src="..." alt="Card image">  <-- code to add a background image to the card (didnt get time to imperlment this)
+        var todayForcast = $("<div>", {     //create the today card
             class: "card bg-dark text-white",
-            style: "height: 190px",
+            style: "height: 215px",
             html: `
             <div class="card-img-overlay">
+                <p class="mb-0">${dayjs.unix(dayForcast.dt).format("D/M/YYYY")}</p>
                 <h5 class="card-title">${dayForcast.name}</h5>
                 <img style="margin: -10px" height=65 src="${iconLink}" />
                 <p class="card-text mb-2">Temp: ${Math.floor(temp)}°C</p>
@@ -104,66 +105,66 @@ $(document).ready(function() {
                 <p class="card-text mb-2">Humidity: ${humidity}</p>
             </div>`
         })
-        $("#today").append(todayForcast)
+        $("#today").append(todayForcast)    //add the today card to he today div
     }
 
-    function displaySearchHistory(){
-        var searchHistory = getLocalStorage("searchHistory")
+    function displaySearchHistory(){    //function to render the search history to the page
+        var searchHistory = getLocalStorage("searchHistory")    //get the search history 
 
-        $("#history").empty();
-        $.each(searchHistory, function(index, searchedCity){
+        $("#history").empty(); //remove anythibng thats currently in the history div
+        $.each(searchHistory, function(index, searchedCity){    //for each item thats in the search histoey create a button
             var searchHistoryBtn = $("<button>", {
-                class: "btn btn-secondary m-1",
-                text: searchedCity
+                class: "btn btn-secondary m-1", //add classes
+                text: searchedCity  //make the button text the name of the city that was searched
             })
-            $("#history").append(searchHistoryBtn)
+            $("#history").append(searchHistoryBtn) //add eaach button the the history div
         })
     }
 
-    function handelButtonClick(e){
-        if(!$(e.target).hasClass('btn')) return
-        e.preventDefault()
+    function handelButtonClick(e){  //function to handel button presses
+        if(!$(e.target).hasClass('btn')) return //if a button wasnt clicked then do nothing
+        e.preventDefault()  //prevent page refresh
         var searchTerm;
         
-        if(e.target.id === "search-button"){
-            searchTerm = $("#search-input").val()
-        } else if($(e.target).hasClass("btn-secondary")) {
-            searchTerm = $(e.target).text()
+        if(e.target.id === "search-button"){    //if it was the search button that was clicked
+            searchTerm = $("#search-input").val()   //get the text from the search box
+        } else if($(e.target).hasClass("btn-secondary")) {  //if it was a search history button clicked
+            searchTerm = $(e.target).text() //get the text from the button
         }
 
-        if(!searchTerm || searchTerm === "") return
+        if(!searchTerm || searchTerm === "") return //if there is no search term or if search term is an empty string then do nothing 
 
         var currentSeatchHistory = getLocalStorage("searchHistory")
-        if(currentSeatchHistory.includes(searchTerm)){
+        if(currentSeatchHistory.includes(searchTerm)){  //if search term is already in search history then remove it as we dont want duplacates
             const indexToBeRemovedAndReaddedToTopOfList = currentSeatchHistory.indexOf(searchTerm);
             currentSeatchHistory.splice(indexToBeRemovedAndReaddedToTopOfList, 1)
         }
-        localStorage.setItem("searchHistory", JSON.stringify([searchTerm, ...currentSeatchHistory]))
+        localStorage.setItem("searchHistory", JSON.stringify([searchTerm, ...currentSeatchHistory])) //add the new search term to the top of the list (first item in the array) and save to local storage
 
-        fetchData(searchTerm, "forecast", build5DayForcast, showError)
-        fetchData(searchTerm, "weather", displayTodayForcast, showError)
+        fetchData(searchTerm, "forecast", build5DayForcast, showError)  //fetch data for the 5 day forcast
+        fetchData(searchTerm, "weather", displayTodayForcast, showError)    //fetch data for the current weather
 
-        displaySearchHistory()
+        displaySearchHistory()  //render the search history buttons
     }
 
-    function getLocalStorage(key){
+    function getLocalStorage(key){  //function to get local storage, if its null then return an empty array
         var data = JSON.parse(localStorage.getItem(key))
         if(!data) data = []
         return data
     }
 
 
-    function initPage(){
+    function initPage(){    //function to handel inital page load
         var searchHistory = getLocalStorage("searchHistory");
-        if(searchHistory.length>0){
+        if(searchHistory.length>0){ //if there is search history then load the weather for the last location searched
             fetchData(searchHistory[0], "forecast", build5DayForcast, showError)
             fetchData(searchHistory[0], "weather", displayTodayForcast, showError)
-        } else {
+        } else {    //if there is no search history then dispay the message below 
             $("#today").append($("<h2>", {class: "m-3", text: "Oh no, looks like you haven't searched for anything yet..."}))
         }
-        displaySearchHistory()
+        displaySearchHistory()  //load search history buttons onto the page
     }
 
-    initPage()
-    $("aside").on("click", handelButtonClick)
+    initPage()  //run the initPage function when the page is loaded
+    $("aside").on("click", handelButtonClick)   //add event listener to the aside section
 })
